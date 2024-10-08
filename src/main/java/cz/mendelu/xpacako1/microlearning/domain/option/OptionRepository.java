aac9ee53a5ccb8777decd95a44be770168158711
L P <xpacako1@mendelu.cz>
2024-10-08 12:20:08 +0200

    cleaning

--- src/main/java/cz/mendelu/xpacako1/microlearning/domain/option/OptionRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.mendelu.xpacako1.microlearning.domain.option;
-
-import cz.mendelu.xpacako1.microlearning.domain.lesson.Lesson;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OptionRepository extends CrudRepository<Option, Long> {
-}
