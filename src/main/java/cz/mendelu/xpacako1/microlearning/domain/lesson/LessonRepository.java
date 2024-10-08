811e0983b930a8df8a8e1f97b9cab5fe5249be18
L P <xpacako1@mendelu.cz>
2024-10-08 12:16:58 +0200

    cleaning

--- src/main/java/cz/mendelu/xpacako1/microlearning/domain/lesson/LessonRepository.java
+++
@@ -1,6 +0,0 @@
-package cz.mendelu.xpacako1.microlearning.domain.lesson;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface LessonRepository extends CrudRepository<Lesson, Long> {
-}
--- src/main/java/cz/mendelu/xpacako1/microlearning/domain/question/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.mendelu.xpacako1.microlearning.domain.question;
-
-import cz.mendelu.xpacako1.microlearning.domain.option.Option;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuestionRepository extends CrudRepository<Question, Long> {
-}
