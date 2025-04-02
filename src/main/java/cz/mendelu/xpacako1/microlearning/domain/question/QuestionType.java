52b8f18079c19a89d2a2dd81398134e1b3330708
L P <xpacako1@mendelu.cz>
2025-04-02 18:12:45 +0200

    from seeder to sql inserts

--- src/main/java/cz/mendelu/xpacako1/microlearning/domain/question/QuestionType.java
+++ src/main/java/cz/mendelu/xpacako1/microlearning/domain/question/QuestionType.java
@@ -1,9 +1,9 @@
 package cz.mendelu.xpacako1.microlearning.domain.question;
 
 public enum QuestionType {
-    ONE_FROM_N, // 1zN,
-    MORE_FROM_N, // MzN
-    CLOZE, // doplnovacka casti do textu, otazka obsahuje vetu s misty k doplneni oznacene
+    ONE_FROM_N, // 1zN,         0
+    MORE_FROM_N, // MzN         1
+    CLOZE, // doplnovacka casti do textu, otazka obsahuje vetu s misty k doplneni oznacene      2
     // dvema hr. zavorkami s cislem uvnitr
-    OPEN // otevrena otazka
+    OPEN // otevrena otazka     3
 }
