a3d00d10d3b309643d174dd4a0b1879c71a5ca1a
L P <xpacako1@mendelu.cz>
2025-05-14 12:48:54 +0200

    mainscreen VM init id of node

--- app/src/main/java/cz/mendelu/pef/microlearning/ui/screens/MainScreenVM.kt
+++ app/src/main/java/cz/mendelu/pef/microlearning/ui/screens/MainScreenVM.kt
@@ -1,20 +1,13 @@
 package cz.mendelu.pef.microlearning.ui.screens
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.wifi.WifiManager
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.core.content.ContextCompat.getSystemService
-import cz.mendelu.pef.microlearning.MainApplication
 import cz.mendelu.pef.microlearning.R
 import cz.mendelu.pef.microlearning.architecture.BaseViewModel
 import cz.mendelu.pef.microlearning.architecture.CommunicationResult
 import cz.mendelu.pef.microlearning.communication.NetworkInterceptor
 import cz.mendelu.pef.microlearning.communication.RemoteRepositoryImpl
-import cz.mendelu.pef.microlearning.model.Node
 import cz.mendelu.pef.microlearning.model.UiState
-import cz.mendelu.pef.microlearning.model.response.ObjectResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -33,7 +26,7 @@ class MainScreenVM @Inject constructor(
 
     var data = MainData()
     var myLLId: Long = 1L
-    var nodeId: Long = 41L
+    var nodeId: Long = 101L
 
 //    private val context = getApplication<Application>().applicationContext
 
