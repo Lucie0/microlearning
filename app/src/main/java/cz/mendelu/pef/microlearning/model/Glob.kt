0c8da357233072efeaf880a58961dda3ad3c1b74
L P <xpacako1@mendelu.cz>
2025-05-19 13:37:38 +0200

    glob todo nodes list + actual node id

--- app/src/main/java/cz/mendelu/pef/microlearning/model/Glob.kt
+++ app/src/main/java/cz/mendelu/pef/microlearning/model/Glob.kt
@@ -9,6 +9,8 @@ var mode = mutableStateOf(Modes.TESTING.name)
 //TESTING + TUITION
 // node_id, node
 var graph: Graph = Graph(0, mutableMapOf())
+var todoNodes = mutableListOf<Long>()
+var actualNodeInGraph: Long = -1L
 var lessonsToStudy = mutableListOf<Long>()
 var walkThroughNodes = listOf<Long>()
 
