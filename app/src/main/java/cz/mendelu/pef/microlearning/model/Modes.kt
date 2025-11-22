2534e62d3833e0f40c6b8797433707ce365e94c8
L P <xpacako1@mendelu.cz>
2025-11-22 15:39:50 +0100

    modes -- supporting text, explanation of each mode

--- app/src/main/java/cz/mendelu/pef/microlearning/model/Modes.kt
+++ app/src/main/java/cz/mendelu/pef/microlearning/model/Modes.kt
@@ -5,13 +5,13 @@ import cz.mendelu.pef.microlearning.R
 
 enum class
 //object
-Modes (var value: String) {
+Modes (var value: String, var supportingText: String) {
 //    REVISION, // uceni pomoci lekci bez testu
 //    TUITION, // uceni pomoci lekci a nasledne vyzkouseni testem
 //    TESTING, // uceni pomoci testu
-    Revision(MainApplication.appContext.getString(R.string.modes_revision)),
-    Tuition(MainApplication.appContext.getString(R.string.modes_tuition)),
-    Testing(MainApplication.appContext.getString(R.string.modes_testing));
+    Revision(MainApplication.appContext.getString(R.string.modes_revision), MainApplication.appContext.getString(R.string.modes_revision_supptext)),
+    Tuition(MainApplication.appContext.getString(R.string.modes_tuition), MainApplication.appContext.getString(R.string.modes_tuition_supptext)),
+    Testing(MainApplication.appContext.getString(R.string.modes_testing),MainApplication.appContext.getString(R.string.modes_testing_supptext));
 
 //    companion object {
 //        val values = listOf(Revision, Tuition, Testing)
--- app/src/main/java/cz/mendelu/pef/microlearning/ui/screens/ModesScreen.kt
+++ app/src/main/java/cz/mendelu/pef/microlearning/ui/screens/ModesScreen.kt
@@ -2,10 +2,17 @@ package cz.mendelu.pef.microlearning.ui.screens
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -37,6 +44,7 @@ fun ModesScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.P)
 @Composable
 fun ModesScreenContent(
@@ -45,13 +53,29 @@ fun ModesScreenContent(
 ){
 //    val selectedOption = remember { mutableStateOf(mode.value) } // vytahnout z D, v jakem modu se nachazi, a dat to jako inicializaci
 
+    val enabled = true
     Column(
         modifier = Modifier.padding(start=16.dp, end=16.dp)
     ){
-        RadioButtonSingleSelection(
-            radioOptions = Modes.values().toList().map { it.name },//.lowercase().replaceFirstChar { ch -> ch.uppercase() } },//"Revision", "Tuition", "Testing"),
-            selectedOption = mode // todo ukladat do DB nastaveny rezim?
-        )
+        Modes.values().forEach {
+            ListItem(
+                modifier = Modifier.clickable { mode.value = it.value },
+                headlineText = { Text(it.value) },
+                supportingText = { Text(it.supportingText) },
+                trailingContent = {
+                    RadioButton(
+                        enabled = enabled,
+                        selected = (it.value == mode.value),
+                        onClick = { mode.value = it.value }
+                    )
+                }
+            )
+        }
+
+//        RadioButtonSingleSelection(
+//            radioOptions = Modes.values().toList().map { it.name },//.lowercase().replaceFirstChar { ch -> ch.uppercase() } },//"Revision", "Tuition", "Testing"),
+//            selectedOption = mode // todo ukladat do DB nastaveny rezim?
+//        )
     }
 
 }
